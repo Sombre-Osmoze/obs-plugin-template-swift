3f107457abc9b6d00804afbeab88b0c1e2487061
Sombre-Osmoze <sombre@osmoze.xyz>
2025-01-18 22:50:03 +0000

    Working module loading

--- Sources/plugin-starter/plugin_starter.swift
+++ Sources/plugin-starter/plugin_starter.swift
@@ -5,10 +5,39 @@ public let PLUGIN_NAME = "starter"
 public let PLUGIN_VERSION = "0.0.1"
 
 
-// TODO: - Document obs module function.
+// MARK: - OBS_DECLARE_MODULE equivalent
+
+nonisolated(unsafe) public var module: UnsafeMutableRawPointer? = nil
+
+@_cdecl("obs_module_ver")
+func obs_module_ver() -> Int32 {
+    print("obs_module_ver")
+    return LIBOBS_API_MAJOR_VER
+}
+
+@_cdecl("obs_module_set_pointer")  
+func obs_module_set_pointer(_ pointer: UnsafeMutableRawPointer) {
+    module = pointer
+    print("obs_module_set_pointer")
+}
+
+func obs_current_module() {
+    print("obs_current_module")
+    // return obs_current_module_pointer()
+}
+
+
+
+
+
+// TODO: - Document obs module function
+
+
 
 @_cdecl("obs_module_load") 
 func obs_module_load() -> Bool {
+    print(FADE_ANIMATION_DURATION)
+
     print("obs_module_load swift")
     return true
 }
